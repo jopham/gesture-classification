3595d17af2a113715ed23336e37fb2bf1b691505
jopham <48107321+jopham@users.noreply.github.com>
2019-03-04 22:10:18 -0500

    Update gesture.py

--- gesture.py
+++ gesture.py
@@ -2,15 +2,16 @@
 RF - gesture classification model
 """
 
-
 ########################
 # IMPORT
 ########################
 import numpy as np
 import pandas as pd
-#from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from scipy.stats import randint
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 from sklearn import metrics
 import time
 
@@ -61,4 +62,34 @@ y_pred = clf_basic.predict(x_test)
 
 # Accuracy
 accuracy = metrics.accuracy_score(y_test, y_pred)
-print("Accuracy: ", accuracy)
+print("Accuracy on the training set: ", accuracy)
+
+########################
+# PARAMETER TUNING
+########################
+
+# Setup the parameters and distributions to sample from: param_dist
+
+param_dist = {"n_estimators": [500], # 500, 1000], #[100, 200],
+              "max_depth": [72], #np.arange(10, 75),
+              "max_features": [3], #randint(1, 12), # np.arange(1, 12), #randint(1, 12)
+              "min_samples_leaf": [0.001], #randint(0.01, 0.1), #[100], #[100, 200, 300, 400, 500],
+              "min_samples_split": [4],
+              "n_jobs": [-1],
+              "criterion": ["gini"]} #"gini", "entropy"]}
+
+print ('start fitting')             # Change this later to write to log
+clf = RandomForestClassifier(n_estimators = 500, min_samples_leaf=0.00075, max_features=3, 
+                             criterion = "gini", 
+                             random_state = seed, n_jobs = -1) # max depth not needed,
+
+# Fit the RF model
+clf.fit(x_train,y_train)
+y_pred = clf.predict(x_test)
+
+y_pred_train = clf.predict(x_train)
+accuracy = metrics.accuracy_score(y_train, y_pred_train)
+print("Accuracy train: ", accuracy)
+
+accuracy = metrics.accuracy_score(y_test, y_pred)
+print("Accuracy on the test set: ", accuracy)       
