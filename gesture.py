ef371464434779c98e3b2681450bd34c0395835c
jopham <48107321+jopham@users.noreply.github.com>
2019-03-07 20:34:58 -0500

    Update gesture.py
    Additional approach

--- gesture.py
+++ gesture.py
@@ -68,6 +68,27 @@ print("Accuracy on the training set: ", accuracy)
 # PARAMETER TUNING
 ########################
 
+# Approach 1: Try cross-validation
+# Setup the parameters and distributions to sample from: param_dist
+param_dist = {"max_depth": [3, None],
+              "max_features": randint(1, 9),
+              "min_samples_leaf": randint(1, 9),
+              "criterion": ["gini", "entropy"]}
+
+# Instantiate a Decision Tree classifier: tree
+tree = DecisionTreeClassifier()
+
+# Instantiate the RandomizedSearchCV object: tree_cv
+tree_cv = RandomizedSearchCV(tree, param_dist, cv=5)
+
+# Fit it to the data
+tree_cv.fit(X,y)
+
+# Print the tuned parameters and score
+print("Tuned Decision Tree Parameters: {}".format(tree_cv.best_params_))
+print("Best score is {}".format(tree_cv.best_score_))
+
+# Approach 2
 # Setup the parameters and distributions to sample from: param_dist
 
 param_dist = {"n_estimators": [500], # 500, 1000], #[100, 200],
